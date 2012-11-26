c0efb5c6d895756191c2beb9226d4db3894033d1
Frime <frime@safri-player.org>
2012-11-26 17:59:14 +0000

    Interfaces IAudioCollection.h & IMediaCollection.h
    git-svn-id: file:///home/safri/trunk@202 b814e8b4-ce5c-4586-8ca0-119f393ed086

---
+++ src/Plugins/CorePlugin/Interfaces/IAudioCollection.h
@@ -0,0 +1,43 @@
+#ifndef IAUDIOCOLLECTION_H
+#define IAUDIOCOLLECTION_H
+#include "../CorePlugin_global.h"
+#include "IMediaCollection.h"
+#include "../CoreData/song.h"
+#include "../CoreData/album.h"
+#include "../CoreData/artist.h"
+#include "../CoreData/genre.h"
+
+namespace Core
+{
+    class COREPLUGINSHARED_EXPORT IAudioCollection : public Core::IMediaCollection
+    {
+        Q_OBJECT
+
+
+    public:
+        virtual QList<Core::Song*> getSongs() = 0;
+        virtual Core::Song* getSongByID(int id) = 0;
+        virtual QList<Core::Song*> getSongsByName(QString name) = 0;
+        virtual Core::Song* getSongByPath(QString path) = 0;
+        virtual Song* newSong(QString name) = 0;
+
+        virtual QList<Core::Artist*> getArtists() = 0;
+        virtual QList<Core::Artist*> getArtistsByName(QString name) = 0;
+        virtual Core::Artist* getArtistByID(int id) = 0;
+        virtual Artist* newArtist(QString name) = 0;
+
+        virtual QList<Core::Album*> getAlbums() = 0;
+        virtual QList<Core::Album*> getAlbumsByName(QString name) = 0;
+        virtual Core::Album* getAlbumByID(int id) = 0;
+        virtual Album* newAlbum(QString album) = 0;
+
+        virtual QList<Core::Genre*> getGenres() = 0;
+        virtual QList<Core::Genre*> getGenresByName(QString name) = 0;
+        virtual Core::Genre* getGenreByID(int id) = 0;
+        virtual Genre* newGenre(QString genre) = 0;
+
+    };
+}
+Q_DECLARE_INTERFACE(Core::IAudioCollection, "Core::IAudioCollection")
+
+#endif // IAUDIOCOLLECTION_H
---
+++ src/Plugins/CorePlugin/Interfaces/IMediaCollection.h
@@ -0,0 +1,41 @@
+#ifndef IMEDIACOLLECTION_H
+#define IMEDIACOLLECTION_H
+
+#include "../CorePlugin_global.h"
+#include "../CoreData/dataitem.h"
+#include "../CoreData/media.h"
+#include <QUrl>
+
+class InfoContainer;
+namespace Core
+{
+    class COREPLUGINSHARED_EXPORT IMediaCollection : public QObject
+    {
+        Q_OBJECT
+        public:
+
+        virtual QList<Core::DataItem*> getByName(DataItem::DATA_ITEM_TYPE type, QString name) = 0;
+        virtual Core::DataItem* getByID(DataItem::DATA_ITEM_TYPE type, int id) = 0;
+        virtual QList<Core::DataItem*> getDataItems(DataItem::DATA_ITEM_TYPE type) = 0;
+        virtual QList<Core::Media*> getMedia() = 0;
+        virtual QString getName() = 0;
+        virtual bool isAvailable() = 0;
+        virtual QUrl getRootPath() = 0;
+        virtual QString getHash() = 0;
+        virtual QUrl getDatabaseLocation() = 0;
+        virtual InfoContainer* getInfoContainerByName(QString name) = 0;
+
+        virtual void lockForWrite() = 0;
+        virtual void lockForRead() = 0;
+        virtual void unlock() = 0;
+
+    signals:
+        void itemAdded(Core::DataItem* item);
+        void itemRemoved(Core::DataItem* item);
+
+    };
+}
+
+Q_DECLARE_INTERFACE(Core::IMediaCollection, "Core::IMediaCollection")
+
+#endif // IMEDIACOLLECTION_H
