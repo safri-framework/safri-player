3b4c7cc5484350b6ba6ced953f918cc1965c0297
Frime <frime@safri-player.org>
2013-08-06 13:44:08 +0000

    komischer selection Rahmen wegmachen, default restclientmode: false
    git-svn-id: file:///home/safri/trunk@429 b814e8b4-ce5c-4586-8ca0-119f393ed086

--- src/Safri-Player/main.cpp
+++ src/Safri-Player/main.cpp
@@ -10,12 +10,26 @@
 #include <QFile>
 #include <QUrl>
 #include <QDir>
+#include <QProxyStyle>
+
+class Style_tweaks : public QProxyStyle
+{
+    public:
+        void drawPrimitive(PrimitiveElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget) const
+        {
+            /* do not draw focus rectangles - this permits modern styling */
+            if (element == QStyle::PE_FrameFocusRect)
+                return;
+            QProxyStyle::drawPrimitive(element, option, painter, widget);
+        }
+};
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
     a.setApplicationName("Safri-Player");
+    a.setStyle(new Style_tweaks());
 
     qDebug() << "\n\nSafri-Player Version: " << SAFRI_VERSION << "";
 
