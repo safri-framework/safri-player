3f54ac25834fb1997a7c5157e79031a69293ac9b
Frime <frime@safri-player.org>
2013-12-05 21:15:56 +0000

    git-svn-id: file:///home/safri/trunk@497 b814e8b4-ce5c-4586-8ca0-119f393ed086

--- src/Plugins/DarkBluePlayerWidget/ProgressCircle.cpp
+++ src/Plugins/DarkBluePlayerWidget/ProgressCircle.cpp
@@ -1,6 +1,14 @@
 #include "ProgressCircle.h"
+#include <QPainter>
+
 
 ProgressCircle::ProgressCircle(QWidget *parent) :
     QWidget(parent)
 {
+    progress = new QImage(":/control/Ressources/control/progress.png");
+}
+
+void ProgressCircle::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
 }
--- src/Plugins/DarkBluePlayerWidget/ProgressCircle.h
+++ src/Plugins/DarkBluePlayerWidget/ProgressCircle.h
@@ -2,16 +2,22 @@
 #define PROGRESSCIRCLE_H
 
 #include <QWidget>
+#include <QImage>
 
 class ProgressCircle : public QWidget
 {
     Q_OBJECT
 public:
     explicit ProgressCircle(QWidget *parent = 0);
-    
+
+private:
+    QImage* progress;
 signals:
     
 public slots:
+
+protected:
+    void paintEvent(QPaintEvent *);
     
 };
 
